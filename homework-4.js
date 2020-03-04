9d2ca43d2d3cee7030e12f9f8baa4543396d4353
hafiz salman <hmsalmans1@gmail.com>
2020-03-04 16:23:57 -0500

    moved files to cloned folder

---
+++ homework-4.js
@@ -0,0 +1,49 @@
+var myQuestions = [
+    {
+        question: "The computer language that makes the pages live is called?",
+        answers: {
+            a: "HTML",
+            b: "Javascript",
+            c: "CSS",
+            d: "None of the above"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "How difficult is the javascript to a newbie like me?",
+        answers: {
+            a: "hard",
+            b: "ver hard",
+            c: " I don't know",
+            d: "Just shut up and learn it! You be fine"
+        },
+        correctAnswer: "d"   
+     },
+     {
+        question: "The library of javascript is?",
+        answers: {
+            a: "HTML",
+            b: "CSS",
+            c: "Jquery",
+            d: "None of the above"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "When was javascript created?",
+        answers: {
+            a: "2005",
+            b: "1887",
+            c: " 1995",
+            d: "2016"
+        },
+        correctAnswer: "c"    }
+
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+
+
